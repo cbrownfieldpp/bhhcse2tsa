8a854e1c57a37a2245dff7e139b070f869ee3761
Cameron Brownfield <cameron@pocketpoints.com>
2019-11-22 13:23:34 -0800

    i3 configures mocha/chai testing and writes 6 example ut for ArrayUtil

---
+++ src/tests/ArrayUtil.test.ts
@@ -0,0 +1,45 @@
+import ArrayUtil from '../lib/ArrayUtil';
+import * as chai from 'chai';
+
+const expect = chai.expect;
+
+describe('ArrayUtil Class', () => {
+    describe('randomSubset()', () => {
+        it('number elements', () => {
+            const testArray: number[] = [1,2,3,4];
+            const subset = ArrayUtil.randomSubset(testArray, 1);
+            expect(typeof(subset[0])).to.equal('number');
+        });
+
+        it('string elements', () => {
+            const testArray: String[] = ['a','b','c','d'];
+            const subset = ArrayUtil.randomSubset(testArray, 1);
+            expect(typeof(subset[0])).to.equal('string');
+        });
+
+        it('object elements', () => {
+            const testArray: any[] = [{ a: 'a' }, { b: 'b' }, { c: 'c' }, { d: 'd', e: 'e' }]
+            const subset = ArrayUtil.randomSubset(testArray, 1);
+            expect(typeof(subset[0])).to.equal('object');
+        });
+
+        it('return array is of size cnt when array size > cnt', () => {
+            const testArray: any[] =  [1, 'b', { c: 'c' }, { d: 'd', e: 'e' }]
+            const subset = ArrayUtil.randomSubset(testArray, 2);
+            expect(subset.length).to.equal(2);
+        })
+
+        it('return array is of size cnt when array size = cnt', () => {
+            const testArray: any[] =  [1, 'b', { c: 'c' }]
+            const subset = ArrayUtil.randomSubset(testArray, 3);
+            expect(subset.length).to.equal(3);
+        })
+
+        it('return array is of size array.length when array size < cnt', () => {
+            const testArray: any[] =  [1, 'b', { c: 'c' }, { d: 'd', e: 'e' }]
+            const subset = ArrayUtil.randomSubset(testArray, 5);
+            expect(subset.length).to.equal(4);
+        })
+
+    });
+});
