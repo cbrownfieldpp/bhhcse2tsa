5ebe5b629e86bded709a5a815ad096f746f420b4
Cameron Brownfield <cameron@pocketpoints.com>
2019-11-26 00:01:32 -0800

    i23 Documentation and Documentation Clean Up

--- .newman/tests/integration-prod.test.js
+++ .newman/tests/integration-prod.test.js
@@ -1,6 +1,15 @@
 const newman = require('newman');
 const dotenv = require('dotenv');
+
+/** This is a functional file for running newman via npm scripts.
+ *  It's also possible run this same function purely via CLI. See nodejs.yml 
+ *  to see an example of that. 
+ */
+
+/** Read .env into dotenv. */
 dotenv.config();
+
+/** Execute newman collection runner. */
 newman.run({
     collection: `https://api.getpostman.com/collections/9563315-53cd1b2f-21de-4e20-b94f-97ca0c7c1f9f?apikey=${process.env.POSTMAN_API_KEY}`,
     environment: `https://api.getpostman.com/environments/9563315-ace70801-6d11-4504-b96a-b2cfabb9abf9?apikey=${process.env.POSTMAN_API_KEY}`,
