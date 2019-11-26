dc7671a87c41fdae2609470bf91d214aa8c6a1e4
Cameron Brownfield <cameron@pocketpoints.com>
2019-11-26 00:13:39 -0800

    i23 Documentation and Documentation Clean Up

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import ArrayUtil from './lib/ArrayUtil';
 
-/** Local scripting can go here. */
+/** Interviewer Note: I've added this method to display how bundle.js is being utilized to run localized JS on the client. 
+ *  At https://cameronbrownfield-bhhcse2tsa.web.app, use the inspector in developer tools to observe the outputted 
+ *  array subset generated from this example function. 
+ */
 const subset: any[] = ArrayUtil.randomSubset([{ a: 'a'}, { b: 'b' }, { c: 'c'}, { d: 'd'}, { e: 'e' }], 3);
 console.log("My ArrayUtil.randomSubset() called from local JS bundle: ", subset);
+
+
+/** Additional Local Scripts */
+
+// ... 
+
+/** End Additional Local Scripts */
